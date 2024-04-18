5ac0357a64088a88d3a8defa6da5aaa0e303a207
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-04-18 18:30:22 +0200

    ready for deploy

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -19,7 +19,7 @@ const ThankYouPage = async ({
   searchParams,
 }: PageProps) => {
   const orderId = searchParams.orderId
-  const  = cookies()
+  const nextCookies = cookies()
 
   const { user } = await getServerSideUser(nextCookies)
   const payload = await getPayloadClient()
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,11 @@ import { appRouter } from './trpc'
 import { inferAsyncReturnType } from '@trpc/server'
 import bodyParser from 'body-parser'
 import { IncomingMessage } from 'http'
-// import { stripeWebhookHandler } from './webhooks'
+import { stripeWebhookHandler } from './webhooks'
 import nextBuild from 'next/dist/build'
 import path from 'path'
 import { PayloadRequest } from 'payload/types'
 import { parse } from 'url'
-import { stripeWebhookHandler } from './webhooks'
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000
@@ -38,7 +37,21 @@ const start = async ()=>{
         webhookMiddleware,
         stripeWebhookHandler
       )
-      
+
+      if (process.env.NEXT_BUILD) {
+        app.listen(PORT, async () => {
+          payload.logger.info(
+            'Next.js is building for production'
+          )
+    
+          // @ts-expect-error
+          await nextBuild(path.join(__dirname, '../'))
+    
+          process.exit()
+        })
+    
+        return
+      }  
     
     const payload = await getPayloadClient({
         initOptions:{
