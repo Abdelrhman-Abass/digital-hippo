75256661db8a536f7a42afddbadf502049616a3e
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-04-18 18:16:41 +0200

    lisitng stripe webhook and create receipt email

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express from  "express"
-import { getPayloadClient } from "./get-payload";
-import { nextApp, nextHandler } from "./next-utils";
+import express from 'express'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './trpc'
-import { inferAsyncReturnType } from "@trpc/server";
+import { inferAsyncReturnType } from '@trpc/server'
+import bodyParser from 'body-parser'
+import { IncomingMessage } from 'http'
+// import { stripeWebhookHandler } from './webhooks'
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
+import { stripeWebhookHandler } from './webhooks'
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000
@@ -12,7 +20,26 @@ const createContext = ({req,res}:trpcExpress.CreateExpressContextOptions) =>({
 })
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
+
+export type WebhookRequest = IncomingMessage & {
+rawBody: Buffer
+}
 const start = async ()=>{
+
+
+    const webhookMiddleware = bodyParser.json({
+        verify: (req: WebhookRequest, _, buffer) => {
+          req.rawBody = buffer
+        },
+      })
+
+      app.post(
+        '/api/webhooks/stripe',
+        webhookMiddleware,
+        stripeWebhookHandler
+      )
+      
+    
     const payload = await getPayloadClient({
         initOptions:{
             express:app,
@@ -21,6 +48,7 @@ const start = async ()=>{
             },
         }
     })
+
 
     app.use(
         '/api/trpc',
